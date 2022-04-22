669a1ffb2d3ac0db2ef7973105e079dd3d397d8c
trajuan <tomer.rajuan@init.de>
2022-04-22 18:20:21 +0200

    restructure in separate components

--- src/App.js
+++ src/App.js
@@ -1,52 +1,19 @@
 import * as React from 'react';
- 
+import List from './components/List';
+import Search from './components/Search';
+
 function getTitle(title) {
   return title;
 }
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
- 
 function App() {
   return (
     <div>
       <h1>Hello {getTitle('React')}</h1>
- 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-      
-      <ul>
-        {list.map((item) =>{
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
+      <Search />
+      <List />
     </div>
   );
 }
- 
+
 export default App;
---
+++ src/components/List.js
@@ -0,0 +1,41 @@
+﻿import React from 'react';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+export default function List() {
+  return (
+    <div>
+      <ul>
+        {list.map((item) => {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
