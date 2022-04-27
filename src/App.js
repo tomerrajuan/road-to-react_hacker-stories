2a0d93b9f529fffdaccd84788fce0efd75ccbb97
trajuan <tomer.rajuan@init.de>
2022-04-27 17:21:24 +0200

    move list array to app.tsx

--- src/App.js
+++ src/App.js
@@ -2,15 +2,35 @@ import * as React from 'react';
 import List from './components/List';
 import Search from './components/Search';
 
+const stories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 const getTitle = (title) => {
   return title;
 };
 
 const App = () => (
+  
   <div>
     <h1>Hello {getTitle('React')}</h1>
     <Search />
-    <List />
+    <List list={stories}/>
   </div>
 );
 
