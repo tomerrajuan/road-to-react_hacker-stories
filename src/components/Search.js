d310f35b8ff03e04f08b87ab37137099123705b5
trajuan <tomer.rajuan@init.de>
2022-04-27 17:21:32 +0200

    useState

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,25 @@
-﻿import React from 'react';
+﻿import React, { useState } from 'react';
 
-const Search = () => (
-  <div>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" />
-  </div>
-);
+const Search = () => {
+  const [inputVal, setInputVal] = useState();
+
+  const handleChange = (event) => {
+    setInputVal(event.target.value);
+    // synthetic event
+    console.log(event);
+    // value of target (here: element)
+    console.log(event.target.value);
+  };
+
+  return (
+    <>
+      <div>
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text" onChange={handleChange} />
+      </div>
+      <p>{inputVal}</p>
+    </>
+  );
+};
 
 export default Search;
