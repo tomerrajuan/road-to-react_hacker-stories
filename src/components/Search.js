5ccaa8bbb56af458d7641ae2ed66374bc33fe698
trajuan <tomer.rajuan@init.de>
2022-05-05 10:18:05 +0200

    [react-fragments] remove unnecessary div

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,15 @@
 ﻿import React from 'react';
 
 const Search = ({ onSearch, search }) => {
-
   return (
     <>
-      <div>
-        <label htmlFor="search">Search: </label>
-        <input
-          id="search"
-          type="text"
-          value={search}
-          onChange={(e) => onSearch(e.target.value)}
-        />
-      </div>
+      <label htmlFor="search">Search: </label>
+      <input
+        id="search"
+        type="text"
+        value={search}
+        onChange={(e) => onSearch(e.target.value)}
+      />
       <p>{search}</p>
     </>
   );
