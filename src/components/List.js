4b51fab7f2a1df579658cc6864d16076940edc2c
trajuan <tomer.rajuan@init.de>
2022-05-06 16:51:09 +0200

    [inline-handler] remove div

--- src/components/List.js
+++ src/components/List.js
@@ -2,13 +2,11 @@
 import Item from './Item';
 
 const List = ({ list, onRemoveItem }) => (
-  <div>
     <ul>
       {list.map((item) => (
         <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
       ))}
     </ul>
-  </div>
 );
 
 export default List;
