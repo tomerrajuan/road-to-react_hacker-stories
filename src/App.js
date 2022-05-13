76a8dc9179398b57e14954e6ebdb6c8b6d491ddc
trajuan <tomer.rajuan@init.de>
2022-05-13 12:59:25 +0200

    [advanced-state] turn to switch case

--- src/App.js
+++ src/App.js
@@ -14,12 +14,16 @@ const useStorageState = (key, initialState) => {
 };
 
 const storiesReducer = (state, action) => {
-  if (action.type === 'SET_STORIES') {
-    return action.payload;
-  } else if (action.type === 'REMOVE_STORY') {
-    return state.filter((story) => action.payload.objectID !== story.objectID);
-  } else {
-    throw new Error();
+  switch (action.payload) {
+    case 'SET_STORIES':
+      return action.payload;
+    case 'REMOVE_STORY':
+      return state.filter(
+        (story) => action.payload.objectID !== story.objectID
+      );
+      
+    default:
+      throw new Error();
   }
 };
 
